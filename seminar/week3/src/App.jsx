eff0d31cf223274219a62a66c78112ced9065509
earl9rey <earl9rey@naver.com>
2025-04-26 16:27:09 +0900

    feat: useState 실습2 - 커스텀 훅 사용

--- seminar/week3/src/App.jsx
+++ seminar/week3/src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
-
+import useSearch from "./hooks/useSearch";
 import { members } from "./member";
 
 function App() {
-  const [search, setSearch] = useState("");
-
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const filteredMembers = members.filter((member) =>
-    member.name.includes(search)
-  );
+  const { search, filteredMembers, handleSearchChange, handleSearch } =
+    useSearch(members);
 
   return (
     <>
       <Header />
-      <Search search={search} handleSearchChange={handleSearch} />
-      <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+      <Search
+        search={search}
+        handleSearchChange={handleSearchChange}
+        handleSearch={handleSearch}
+      />
+      <section style={{ display: "flex", flexWrap: "wrap" }}>
         {filteredMembers.map((member) => (
           <Card
             key={member.id}
             name={member.name}
-            englishName={member.englishName}
             github={member.github}
+            englishName={member.englishName}
           />
         ))}
       </section>
--- seminar/week3/src/components/Search.jsx
+++ seminar/week3/src/components/Search.jsx
@@ -1,7 +1,15 @@
-const Search = ({ search, handleSearchChange }) => {
+import React from "react";
+
+const Search = ({ search, handleSearchChange, handleSearch }) => {
   return (
     <div>
-      <input type="text" value={search} onChange={handleSearchChange} />
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="검색어를 입력하세요"
+      />
+      <button onClick={handleSearch}>검색</button>
     </div>
   );
 };
