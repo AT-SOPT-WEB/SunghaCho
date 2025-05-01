439e37f081babef839dd43ff23b08b8c3ab47aac
earl9rey <earl9rey@naver.com>
2025-05-01 18:22:10 +0900

    feat: 게임 로직 구현

--- assignment/week3/src/components/Game.jsx
+++ assignment/week3/src/components/Game.jsx
@@ -1,26 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./game/Input";
 import Message from "./game/Message";
 import List from "./game/List";
 
 const Game = () => {
+  const [secretNum, setSecretNum] = useState([]);
+
+  useEffect(() => {
+    const numList = [];
+    while (numList.length < 3) {
+      const rand = Math.floor(Math.random() * 10);
+      if (!numList.includes(rand)) {
+        numList.push(rand);
+      }
+    }
+
+    setSecretNum(numList);
+  }, []);
+
+  console.log(secretNum);
+
+  function checkAnswer(input, secretNum) {
+    const inputNum = input.split("").map(Number);
+
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (inputNum[i] === secretNum[i]) {
+        strike++;
+      } else if (secretNum.includes(inputNum[i])) {
+        ball++;
+      }
+    }
+
+    return { strike, ball };
+  }
+
   const [inputValue, setInputValue] = useState(null);
-  const [message, setMessage] = useState(null);
+  const [isValid, setIsValid] = useState(true);
 
   const handleInputSubmit = (value, valid) => {
     if (valid) {
       setInputValue(value);
-      setMessage("스트라이크/볼/아웃");
+      setIsValid(valid);
     } else {
-      setMessage("서로 다른 숫자 3자리를 입력해주세요.");
+      setIsValid(valid);
     }
   };
+
+  const result =
+    inputValue && inputValue.length === 3 && isValid
+      ? checkAnswer(inputValue, secretNum)
+      : "";
 
   return (
     <div>
       <Input onSubmit={handleInputSubmit} />
-      <Message message={message} />
-      <List inputValue={inputValue} />
+      <Message isValid={isValid} result={result} />
+      <List inputValue={inputValue} result={result} />
     </div>
   );
 };
