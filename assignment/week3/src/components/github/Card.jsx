8532c403c0d1bc4b7c1db5641ff27ee985e72170
earl9rey <earl9rey@naver.com>
2025-05-01 21:26:53 +0900

    feat: 사용자 이름/이미지 클릭 시 URL 이동

--- assignment/week3/src/components/github/Card.jsx
+++ assignment/week3/src/components/github/Card.jsx
@@ -57,8 +57,10 @@ const Card = ({ profile, onClose }) => {
       {/* 조건부 렌더링을 통해 resolved 상태 즉, 검색 결과 데이터가 있는 경우에 결과를 표시하는겁니다. */}
       {userInfo.status === "resolved" && (
         <div>
-          <img src={userInfo.data.avatar_url} />
-          <p>{userInfo.data.name}</p>
+          <a href={userInfo.data.html_url} target="_blank">
+            <img src={userInfo.data.avatar_url} />
+            <p>{userInfo.data.name}</p>
+          </a>
           <p>한 줄소개: {userInfo.data.bio}</p>
           <p>팔로워: {userInfo.data.followers}</p>
           <p>팔로잉: {userInfo.data.following}</p>
