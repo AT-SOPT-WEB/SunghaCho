8c8eda4eb7ba1918ef0c56f867d81636bf7618f3
earl9rey <earl9rey@naver.com>
2025-05-01 18:23:05 +0900

    feat: Message - 게임 결과 출력

--- assignment/week3/src/components/game/Message.jsx
+++ assignment/week3/src/components/game/Message.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Message = ({ message }) => {
-  return <div>{message}</div>;
+const Message = ({ isValid, result }) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!result) return;
+    const { strike, ball } = result;
+
+    if (strike === 0 && ball === 0) {
+      setMessage("OUT");
+    } else if (strike === 3) {
+      setMessage("정답입니다! 3초 뒤에 게임이 리셋됩니다.");
+    } else {
+      setMessage(`${strike}S ${ball}B`);
+    }
+  }, [result]);
+
+  return (
+    <>
+      {!isValid && <div>서로 다른 숫자 3자리를 입력해주세요.</div>}
+      {isValid && <div>{message}</div>}
+    </>
+  );
 };
 
 export default Message;
