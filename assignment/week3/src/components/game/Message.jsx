9f69003d6e813510fe9806957aca68c8ef2436e8
earl9rey <earl9rey@naver.com>
2025-05-02 11:15:26 +0900

    fix: 게임 시간 초과 조건부 처리 오류 해결

--- assignment/week3/src/components/game/Message.jsx
+++ assignment/week3/src/components/game/Message.jsx
@@ -50,14 +50,14 @@ const Message = ({ isValid, result, count }) => {
     if (!result) return;
     const { strike, ball } = result;
 
-    if (strike === 0 && ball === 0) {
-      setMessage("OUT");
-    } else if (strike === 3) {
-      setMessage("정답입니다! 3초 뒤에 게임이 리셋됩니다.");
-    } else if (count === 10) {
+    if (count === 10) {
       setMessage(
         "게임 오버! 10번을 넘겨서 실패하였습니다. 게임이 초기화됩니다."
       );
+    } else if (strike === 3) {
+      setMessage("정답입니다! 3초 뒤에 게임이 리셋됩니다.");
+    } else if (strike === 0 && ball === 0) {
+      setMessage("OUT");
     } else {
       setMessage(`${strike} 스트라이크 ${ball} 볼`);
     }
