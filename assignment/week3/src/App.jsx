d0d3a3b18fc28cc93fa70fe37dd31e9606f259ec
earl9rey <earl9rey@naver.com>
2025-05-02 10:46:26 +0900

    style: 전역 스타일 추가

--- assignment/week3/src/App.jsx
+++ assignment/week3/src/App.jsx
@@ -2,16 +2,50 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Game from "./components/Game";
 import Github from "./components/Github";
+/** @jsxImportSource @emotion/react */
+import { Global, css } from "@emotion/react";
+
+const container = css`
+  width: 100vw;
+  display: grid;
+  background-color: #eaeaea;
+`;
+
+const wrapper = css`
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  justify-items: center;
+  margin-top: 10px;
+  background-color: #eaeaea;
+`;
 
 function App() {
-  const [activeTab, setActiveTab] = useState(null);
+  const [activeTab, setActiveTab] = useState("github");
   return (
-    <>
-      <Header onTabChange={setActiveTab} />
+    <div css={container}>
+      <Global
+        styles={css`
+          @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css");
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Pretendard Variable", Pretendard, sans-serif;
+          }
 
-      {activeTab === "github" && <Github />}
-      {activeTab === "game" && <Game />}
-    </>
+          body {
+            background-color: #fff;
+            color: #000;
+          }
+        `}
+      />
+      <Header onTabChange={setActiveTab} tab={activeTab} />
+      <div css={wrapper}>
+        {activeTab === "github" && <Github />}
+        {activeTab === "game" && <Game />}
+      </div>
+    </div>
   );
 }
 
