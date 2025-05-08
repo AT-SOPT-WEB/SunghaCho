f100d8885698d59791a0f945def2520fe9c9d51d
earl9rey <earl9rey@naver.com>
2025-05-08 20:33:09 +0900

    feat: default 경로 설정 (/login)

--- assignment/week4/src/router/Router.tsx
+++ assignment/week4/src/router/Router.tsx
@@ -1,7 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import Login from "../pages/Login";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" />, // 기본 경로에서 /login으로 리다이렉트
+  },
   {
     path: "/login",
     element: <Login />,
