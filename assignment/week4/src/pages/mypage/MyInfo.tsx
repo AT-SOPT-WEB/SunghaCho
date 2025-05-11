dcd5fc6f4e7efd333eff37d21b022e79d3d61ba2
earl9rey <earl9rey@naver.com>
2025-05-11 14:35:42 +0900

    feat: 마이페이지 버튼 활성화 구현 및 input 상태 관리

--- assignment/week4/src/pages/mypage/MyInfo.tsx
+++ assignment/week4/src/pages/mypage/MyInfo.tsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from "react";
 import Header from "../../components/mypage/Header";
 import Container from "../../components/styled/Container";
 import Input from "../../components/styled/Input";
 import Button from "../../components/styled/Button";
 
 const MyInfo = () => {
+  const [newNickname, setNewNickname] = useState("");
+  const [isBtnEnable, setIsBtnEnable] = useState(false);
+
+  useEffect(() => {
+    setIsBtnEnable(!newNickname);
+  }, [newNickname]);
+
   return (
     <>
       <Header />
       <Container>
         <h2>내 정보 수정하기</h2>
         <p>새 닉네임</p>
-        <Input placeholder="새 닉네임을 입력하세요" />
-        <Button>저장</Button>
+        <Input
+          placeholder="새 닉네임을 입력하세요"
+          value={newNickname}
+          onChange={(e) => setNewNickname(e.target.value)}
+        />
+        <Button disabled={isBtnEnable}>변경사항 저장</Button>
       </Container>
     </>
   );
--- assignment/week4/src/pages/mypage/MySearch.tsx
+++ assignment/week4/src/pages/mypage/MySearch.tsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from "react";
 import Header from "../../components/mypage/Header";
 import Container from "../../components/styled/Container";
 import Input from "../../components/styled/Input";
 import Button from "../../components/styled/Button";
 
 const MySearch = () => {
+  const [searchVal, setSearchVal] = useState("");
+  const [isBtnEnable, setIsBtnEnable] = useState(false);
+
+  useEffect(() => {
+    setIsBtnEnable(!searchVal);
+  }, [searchVal]);
   return (
     <>
       <Header />
       <Container>
         <h2>Sopt 회원 조회하기</h2>
         <p>닉네임</p>
-        <Input placeholder="검색할 닉네임을 입력하세요" />
-        <Button>확인</Button>
+        <Input
+          placeholder="검색할 닉네임을 입력하세요"
+          value={searchVal}
+          onChange={(e) => setSearchVal(e.target.value)}
+        />
+        <Button disabled={isBtnEnable}>조회</Button>
       </Container>
     </>
   );
