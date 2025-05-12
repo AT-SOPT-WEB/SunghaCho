2b2944bf919947a73a15f150427a0c1ac4094fe9
earl9rey <earl9rey@naver.com>
2025-05-12 21:45:37 +0900

    feat: 회원 조회 (검색) api 연동

--- assignment/week4/src/pages/mypage/MySearch.tsx
+++ assignment/week4/src/pages/mypage/MySearch.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 import Header from "../../components/mypage/Header";
 import Container from "../../components/styled/Container";
 import Input from "../../components/styled/Input";
@@ -6,11 +7,20 @@ import Button from "../../components/styled/Button";
 
 const MySearch = () => {
   const [searchVal, setSearchVal] = useState("");
-  const [isBtnEnable, setIsBtnEnable] = useState(false);
+  const [nicknameList, setNicknameList] = useState([]);
 
-  useEffect(() => {
-    setIsBtnEnable(!searchVal);
-  }, [searchVal]);
+  const getSearch = async () => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URL}/api/v1/users?keyword=${searchVal}`
+      );
+      console.log(res.data);
+      setNicknameList(res.data.data.nicknameList);
+    } catch (error) {
+      console.error("검색 실패", error);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -22,7 +32,8 @@ const MySearch = () => {
           value={searchVal}
           onChange={(e) => setSearchVal(e.target.value)}
         />
-        <Button disabled={isBtnEnable}>조회</Button>
+        <Button onClick={() => getSearch()}>조회</Button>
+        <div>{nicknameList}</div>
       </Container>
     </>
   );
