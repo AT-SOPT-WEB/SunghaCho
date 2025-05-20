06686db8214760bb34d1ad0a5521c2a102ea361c
earl9rey <earl9rey@naver.com>
2025-05-21 01:52:30 +0900

    feat: 비밀번호 표시/감추기 구현 (심화)

--- assignment/week4/src/components/login/SignUpPwd.tsx
+++ assignment/week4/src/components/login/SignUpPwd.tsx
@@ -1,14 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { useState, useEffect } from "react";
 import Button from "../styled/Button";
 import Input from "../styled/Input";
 import ErrorMessage from "../styled/ErrorMessage";
 import Subtitle from "../styled/Subtitle";
 import type { SignUpPwdProps } from "@/types/props/auth";
+import hideIcon from "../../assets/hideIcon.svg"
+import showIcon from "../../assets/showIcon.svg"
+
+const divStyle = css`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 30rem;
+  border: 1px solid blue;
+`;
+
+const showPwdStyle = css`
+  position: absolute;
+  width : 25px;
+  height : 25px;
+  right : 1.1rem;
+`;
+
+const hidePwdStyle = css`
+  position: absolute;
+  width : 28px;
+  height : 27px;
+  right : 1rem;
+`;
 
 const SignUpPwd = ({ newPwd, setNewPwd, handleSignupStep }: SignUpPwdProps) => {
   const [checkPwd, setCheckPwd] = useState<string>("");
   const [isBtnDisable, setIsBtnDisable] = useState<boolean>(true);
   const [errorMsg, setErrorMsg] = useState<string>("");
+  const [isVisiblePwd, setIsVisiblePwd] = useState<boolean>(false)
 
   useEffect(() => {
     if (newPwd.length > 20) {
@@ -33,17 +60,23 @@ const SignUpPwd = ({ newPwd, setNewPwd, handleSignupStep }: SignUpPwdProps) => {
     setErrorMsg("");
   }, [newPwd, checkPwd]);
 
+  const handleClickShowPwd = () => {
+    setIsVisiblePwd(prev => !prev);
+  }
+
   return (
     <>
       <Subtitle>비밀번호</Subtitle>
+      <div css={divStyle}>
       <Input
-        type="password"
+        type={isVisiblePwd ? "text" : "password"} 
         placeholder="비밀번호를 입력해주세요"
         value={newPwd}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setNewPwd(e.target.value)
         }
       />
+{ isVisiblePwd ? <img src={hideIcon} css={hidePwdStyle} onClick={() => handleClickShowPwd()}  alt="비밀번호 감추기 아이콘"/> : <img src={showIcon} css={showPwdStyle} onClick={() => handleClickShowPwd()} alt="비밀번호 표시 아이콘"/> }</div>
       <Input
         placeholder="비밀번호 확인"
         value={checkPwd}
