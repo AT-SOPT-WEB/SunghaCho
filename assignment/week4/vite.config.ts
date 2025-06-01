48c8c0d325b7841167d4959cb982431c1136e4ea
earl9rey <earl9rey@naver.com>
2025-06-01 21:16:19 +0900

    refactor: 절대경로 추가

--- assignment/week4/vite.config.ts
+++ assignment/week4/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@api': path.resolve(__dirname, 'src/api'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@types': path.resolve(__dirname, 'src/types'),
     },
   },
 });
